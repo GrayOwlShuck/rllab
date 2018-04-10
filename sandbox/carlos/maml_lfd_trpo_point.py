031e517a5d783e11ef68c2937df2e9ece51ed549
florensacc <florensacc@berkeley.edu>
2018-04-09 23:00:54 -0700

    point env and launch file

---
+++ sandbox/carlos/maml_lfd_trpo_point.py
@@ -0,0 +1,193 @@
+import datetime
+import os.path as osp
+from rllab.misc import logger
+from sandbox.young_clgan.logging import HTMLReport
+import tensorflow as tf
+from sandbox.rocky.tf.algos.sensitive_lfd_trpo import SensitiveLfD_TRPO
+from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
+from rllab.baselines.gaussian_mlp_baseline import GaussianMLPBaseline
+from rllab.baselines.zero_baseline import ZeroBaseline
+from examples.point_env_randgoal import PointEnvRandGoal, StraightDemo
+from rllab.envs.normalized_env import normalize
+from rllab.misc.instrument import run_experiment_lite
+from sandbox.rocky.tf.policies.sens_lfd_minimal_gauss_mlp_policy import SensitiveLfdGaussianMLPPolicy
+from sandbox.rocky.tf.envs.base import TfEnv
+from sandbox.young_clgan.logging import HTMLReport
+from sandbox.young_clgan.logging import format_dict
+from rllab.misc.instrument import VariantGenerator
+
+import argparse
+import random
+import sys
+from multiprocessing import cpu_count
+from rllab import config
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--ec2', '-e', action='store_true', default=False, help="add flag to run in ec2")
+parser.add_argument('--local_docker', '-d', action='store_true', default=False,
+                    help="add flag to run in local dock")
+parser.add_argument('--type', '-t', type=str, default='', help='set instance type')
+parser.add_argument('--price', '-p', type=str, default='', help='set betting price')
+parser.add_argument('--subnet', '-sn', type=str, default='', help='set subnet like us-west-1a')
+parser.add_argument('--name', '-n', type=str, default='', help='set exp prefix name and new file name')
+parser.add_argument('--debug', action='store_true', default=False, help="run code without multiprocessing")
+args = parser.parse_args()
+
+# setup ec2
+subnets = [
+    'us-east-2c', 'us-east-2b', 'us-east-2a', 'us-west-1a', 'ap-southeast-1a', 'us-west-1b',
+    'ap-southeast-1b', 'ap-northeast-2c', 'eu-west-1c', 'eu-west-1b', 'ap-northeast-2a'
+]
+ec2_instance = args.type if args.type else 'm4.large'
+# configure instance
+info = config.INSTANCE_TYPE_INFO[ec2_instance]
+config.AWS_INSTANCE_TYPE = ec2_instance
+config.AWS_SPOT_PRICE = str(info["price"])
+n_parallel = int(info["vCPU"] / 2)  # make the default 4 if not using ec2
+if args.ec2:
+    mode = 'ec2'
+elif args.local_docker:
+    mode = 'local_docker'
+    n_parallel = cpu_count() if not args.debug else 1
+else:
+    mode = 'local'
+    n_parallel = cpu_count() if not args.debug else 1
+
+
+vg = VariantGenerator()
+# fast updates
+vg.add('demo_batch_size', [1, 5, 10, 15, 20])  # numb of traj!
+vg.add('batch_size', [20])  # numb of traj!
+vg.add('baseline', ['linear'])
+vg.add('max_path_length', [100])
+vg.add('num_grad_updates', [1, 2, 3, 4, 5])
+vg.add('fast_learning_rate', [0.5])
+# meta
+vg.add('use_meta', [True])  # if False it won't update the initial params from one itr to the next
+vg.add('meta_itr', lambda use_meta: [30] if use_meta else [1])
+vg.add('meta_step_size', [0.01])
+vg.add('meta_batch_size', [40])  # 10 works but much less stable, 20 is fairly stable, 40 is more stable --> n_env!!!!
+# env
+vg.add('env_noise', [0, 0.5])
+vg.add('tolerance', [0.1])
+vg.add('seed', range(0, 20, 10))
+EXPERIMENT_TYPE = osp.basename(__file__).split('.')[0]
+
+
+def run_task(v):
+
+    logger.log("Initializing report...")
+    report = HTMLReport(osp.join(logger.get_snapshot_dir(), 'report.html'), images_per_row=5)
+    report.add_header("{}".format(EXPERIMENT_TYPE))
+    report.add_text(format_dict(v))
+    report.save()
+
+    env = TfEnv(normalize(PointEnvRandGoal(noise=v['env_noise'], tolerance=v['tolerance'])))
+    policy = SensitiveLfdGaussianMLPPolicy(
+        name="policy",
+        env_spec=env.spec,
+        grad_step_size=v['fast_learning_rate'],
+        hidden_nonlinearity=tf.nn.relu,
+        hidden_sizes=(100, 100),
+    )
+
+    demo_policy = StraightDemo(env_spec=env.spec)
+
+    if v['baseline'] == 'zero':
+        baseline = ZeroBaseline(env_spec=env.spec)
+    elif 'linear' in v['baseline']:
+        baseline = LinearFeatureBaseline(env_spec=env.spec)
+    else:
+        baseline = GaussianMLPBaseline(env_spec=env.spec)
+
+    algo = SensitiveLfD_TRPO(
+        env=env,
+        policy=policy,
+        demo_policy=demo_policy,
+        baseline=baseline,
+        batch_size=v['batch_size'],  # number of trajs for grad update
+        demo_batch_size=v['demo_batch_size'],
+        max_path_length=v['max_path_length'],
+        meta_batch_size=v['meta_batch_size'],
+        num_grad_updates=v['num_grad_updates'],
+        n_itr=v['meta_itr'],
+        use_meta=v['use_meta'],
+        step_size=v['meta_step_size'],
+        plot=False,
+        report=report,
+        preupdate_samples=True,
+    )
+
+    algo.train()
+
+
+exp_prefix = 'lfd_mesh_grad_demos'
+print("\n" + "**********" * 10 + "\nexp_prefix: {}\nvariants: {}".format(exp_prefix, vg.size))
+print('Running on type {}, with price {}, parallel {} on the subnets: '.format(config.AWS_INSTANCE_TYPE,
+                                                                               config.AWS_SPOT_PRICE, n_parallel),
+      *subnets)
+for vv in vg.variants():
+    if mode in ['ec2', 'local_docker']:
+        # choose subnet
+        subnet = random.choice(subnets)
+        config.AWS_REGION_NAME = subnet[:-1]
+        config.AWS_KEY_NAME = config.ALL_REGION_AWS_KEY_NAMES[
+            config.AWS_REGION_NAME]
+        config.AWS_IMAGE_ID = config.ALL_REGION_AWS_IMAGE_IDS[
+            config.AWS_REGION_NAME]
+        config.AWS_SECURITY_GROUP_IDS = \
+            config.ALL_REGION_AWS_SECURITY_GROUP_IDS[
+                config.AWS_REGION_NAME]
+        config.AWS_NETWORK_INTERFACES = [
+            dict(
+                SubnetId=config.ALL_SUBNET_INFO[subnet]["SubnetID"],
+                Groups=config.AWS_SECURITY_GROUP_IDS,
+                DeviceIndex=0,
+                AssociatePublicIpAddress=True,
+            )
+        ]
+        run_experiment_lite(
+            # use_cloudpickle=True,
+            stub_method_call=run_task,
+            variant=vv,
+            mode=mode,
+            n_parallel=4,
+            snapshot_mode="last",
+            seed=vv['seed'],
+            exp_prefix=exp_prefix,
+            # exp_name='tf1lfd_trposens' + str(int(use_sensitive)) + '_fbs' + str(fast_batch_size) + '_mbs' + str(
+            #     meta_batch_size) + '_flr_' + str(fast_learning_rate) + 'metalr_' + str(
+            #     meta_step_size) + '_env_noise' + str(env_noise) + '_step1' + str(num_grad_updates) + '_s' + str(s),
+            plot=False,
+            sync_s3_pkl=True,
+            sync_s3_html=True,
+            # # use this ONLY with ec2 or local_docker!!!
+            pre_commands=[
+                'pip install --upgrade pip',
+                'pip install --upgrade cloudpickle',
+                'export MPLBACKEND=Agg',
+                'pip install --upgrade -I tensorflow',
+                'pip install git+https://github.com/tflearn/tflearn.git',
+                'pip install dominate',
+                # # 'pip install multiprocessing_on_dill',
+                'pip install scikit-image',
+                'conda install numpy -n rllab3 -y',
+            ],
+        )
+    else:
+        run_experiment_lite(
+            # use_cloudpickle=False,
+            stub_method_call=run_task,
+            variant=vv,
+            mode=mode,
+            n_parallel=4,
+            snapshot_mode="last",
+            seed=vv['seed'],
+            exp_prefix='lfd_mesh_grad_demos',
+            # exp_name='tf1lfd_trposens' + str(int(use_sensitive)) + '_fbs' + str(fast_batch_size) + '_mbs' + str(
+            #     meta_batch_size) + '_flr_' + str(fast_learning_rate) + 'metalr_' + str(
+            #     meta_step_size) + '_env_noise' + str(env_noise) + '_step1' + str(num_grad_updates) + '_s' + str(s),
+            plot=False,
+        )
+
---
+++ sandbox/carlos/point_env_randgoal.py
@@ -0,0 +1,242 @@
+import os.path as osp
+import tempfile
+import scipy.misc
+from rllab.envs.base import Env
+from rllab.spaces import Box
+from rllab.envs.base import Step
+from rllab.misc import logger
+import numpy as np
+from sandbox.rocky.tf.policies.base import Policy
+from rllab.misc.overrides import overrides
+from rllab.core.serializable import Serializable
+import matplotlib.pyplot as plt
+
+# from pylab import *
+import matplotlib.colorbar as cbar
+import matplotlib.patches as patches
+
+
+class PointEnvRandGoal(Env):
+    def __init__(self, goal=None, noise=0.1, tolerance=0.1):  # Can set goal to test adaptation.
+        self._goal = goal
+        self._state = (0, 0)
+        self._noise = noise
+        self._tolerance = tolerance
+
+    @property  # this should have the same name for all: ant/cheetah direction,... and be a list of params
+    def objective_params(self):
+        return self._goal
+
+    @property
+    def observation_space(self):
+        return Box(low=-8, high=8, shape=(2,))
+
+    @property
+    def action_space(self):
+        return Box(low=-0.1, high=0.1, shape=(2,))
+
+    def reset(self, objective_params=None, clean_reset=False):
+        if clean_reset:
+            # print("cleaning goal")
+            self._goal = None
+        else:
+            if objective_params is not None:
+                # print("using given goal")
+                self._goal = objective_params
+            elif self._goal is None:
+                # Only set a new goal if this env hasn't had one defined before or if it has been cleaned
+                self._goal = np.random.uniform(-5, 5, size=(2,))
+        self._state = (0, 0)
+        observation = np.copy(self._state)
+        return observation
+
+    def step(self, action):
+        # print("inside env step: _state, goal, action:", self._state, self._goal, self. action)
+        self._state = np.clip(self._state + action + self._noise * np.random.randn(*np.shape(self._state)),
+                              *self.observation_space.bounds)
+        x, y = self._state
+        x -= self._goal[0]
+        y -= self._goal[1]
+        reward = - (x ** 2 + y ** 2) ** 0.5
+        done = abs(x) < 0.1 and abs(y) < 0.1
+        next_observation = np.copy(self._state)
+        return Step(observation=next_observation, reward=reward, done=done, goal=self._goal, goal_reached=done)  # goal goes to env_infos
+
+    def render(self):
+        print('current state:', self._state)
+
+    def log_diagnostics(self, postupdate_paths, demos=None, preupdate_paths=None, prefix='', report=None, policy=None, goals=None):
+        """
+        :param postupdate_paths: list of dicts, one per gradient update. The keys of the dict are the env numb and the val the UNprocessed samples
+        :param demos: same
+        """
+        logger.log("Saving visualization of paths")
+        if type(postupdate_paths) == dict:
+            postupdate_paths = postupdate_paths
+            preupdate_paths = preupdate_paths
+        elif (type(postupdate_paths) == list and len(postupdate_paths) > 1 and type(postupdate_paths[0]) == dict):
+            preupdate_paths = postupdate_paths[0]  # paths obtained before any update
+            postupdate_paths = postupdate_paths[-1]  # paths obtained after all updates
+        else:
+            raise ValueError("log_diagnostics only supports paths from vectorized sampler!")
+
+        num_envs = len(postupdate_paths)
+        avg_postupdate_rewards = [None] * num_envs
+        avg_postupdate_success = [None] * num_envs
+        avg_preupdate_rewards = [None] * num_envs
+        avg_preupdate_success = [None] * num_envs
+        avg_demo_rewards = [None] * num_envs
+        avg_demo_success = [None] * num_envs
+        for ind in range(num_envs):
+            avg_postupdate_rewards[ind] = np.mean([np.mean(p['rewards']) for p in postupdate_paths[ind]])
+            avg_postupdate_success[ind] = np.mean([np.any(p['env_infos']['goal_reached']) for p in postupdate_paths[ind]])
+            if demos is not None:
+                avg_demo_rewards[ind] = np.mean([np.mean(p['rewards']) for p in demos[ind]])
+                avg_demo_success[ind] = np.mean([np.any(p['env_infos']['goal_reached']) for p in demos[ind]])
+            if preupdate_paths is not None:
+                avg_preupdate_rewards[ind] = np.mean([np.mean(p['rewards']) for p in preupdate_paths[ind]])
+                avg_preupdate_success[ind] = np.mean([np.any(p['env_infos']['goal_reached']) for p in preupdate_paths[ind]])
+
+        for ind in range(min(5, num_envs)):
+            plt.clf()
+            plt.plot(*postupdate_paths[ind][0]['env_infos']['goal'][0], 'r*',
+                     markersize=10)
+            post_points = postupdate_paths[ind][0]['observations']
+            plt.plot(post_points[:, 0], post_points[:, 1], '-b', linewidth=1)
+
+            # pre_points = preupdate_paths[ind][1]['observations']
+            # post_points = postupdate_paths[ind][1]['observations']
+            # plt.plot(pre_points[:, 0], pre_points[:, 1], '--r', linewidth=2)
+            # plt.plot(post_points[:, 0], post_points[:, 1], '--b', linewidth=1)
+            #
+
+            if demos is not None:
+                if type(demos) == dict:
+                    demo_paths = demos
+                else:  # todo: in what case is demos a list? what are the other elements?
+                    demo_paths = demos[0]
+                demo_points = demo_paths[ind][0]['observations']
+                plt.plot(demo_points[:, 0], demo_points[:, 1], '-.g', linewidth=2)
+
+            if preupdate_paths is not None:
+                pre_points = preupdate_paths[ind][0]['observations']
+                plt.plot(pre_points[:, 0], pre_points[:, 1], '-m', linewidth=1)
+                plt.legend(['goal', 'pre-update path', 'demos', 'post-update path'])
+            else:
+                plt.legend(['goal', 'post-update path', 'demos'])
+
+            plt.plot(0, 0, 'b.', markersize=5)
+            plt.xlim([-8, 8])
+            plt.ylim([-8, 8])
+
+            log_dir = logger.get_snapshot_dir()
+            if report is None:
+                plt.savefig(osp.join(log_dir, 'post_update_plot' + str(ind) + '.png'))
+            else:
+                vec_img = save_image()
+                summary_text = 'Env_{}:\nAvg Postupdate Rew: {:.2f}, Avg Post Success:{:.2f}\n'.format(ind,
+                                                                                      avg_postupdate_rewards[ind],
+                                                                                      avg_postupdate_success[ind])
+                if avg_demo_rewards[ind] is not None:
+                    summary_text += 'Avg Demo Rew: {:.2f}, Avg Demo Success:{:.2f}\n'.format(avg_demo_rewards[ind],
+                                                                                     avg_demo_success[ind])
+                if avg_preupdate_rewards[ind] is not None:
+                    summary_text += 'Avg Preupdate Rew: {:.2f}, Avg Preupdate Success:{:.2f}\n'.format(avg_preupdate_rewards[ind],
+                                                                                               avg_preupdate_success[ind])
+                report.add_image(vec_img, summary_text)
+
+        report.new_row()
+        if policy is not None:
+            plot_policy_means(policy, self, self.observation_space.bounds, report, goals=goals)
+
+        summary_text = 'Avg Postupdate Rew: {:.2f}, Avg Post Success:{:.2f}\n'.format(np.mean(avg_postupdate_rewards),
+                                                                              np.mean(avg_postupdate_success))
+        logger.record_tabular('postUpdate_AverageSuccess', np.mean(avg_postupdate_success))
+        if avg_demo_rewards[0] is not None:
+            summary_text += 'Avg Demo Rew: {:.2f}, Avg Demo Success:{:.2f}\n'.format(np.mean(avg_demo_rewards),
+                                                                             np.mean(avg_demo_success))
+            logger.record_tabular('demo_AverageSuccess', np.mean(avg_demo_success))
+        if avg_preupdate_rewards[0] is not None:
+            summary_text += 'Avg Preupdate Rew: {:.2f}, Avg Preupdate Success:{:.2f}\n'.format(np.mean(avg_preupdate_rewards),
+                                                                                       np.mean(avg_preupdate_success))
+            logger.record_tabular('preUpdate_AverageSuccess', np.mean(avg_preupdate_success))
+        report.add_text(summary_text)
+        report.save()
+
+
+class StraightDemo(Policy, Serializable):
+    def __init__(self, *args, **kwargs):
+        Serializable.quick_init(self, locals())
+        super(StraightDemo, self).__init__(*args, **kwargs)
+
+    @overrides
+    def get_action(self, observation, objective_params, *args,
+                   **kwargs):  # the same policy obj is applied to all envs!! so it needs to take in the goal!
+        goal_vec = np.array(objective_params) - np.array(observation)
+        return np.clip(goal_vec, *self.action_space.bounds), dict()
+
+    @overrides
+    def get_actions(self, observations, objective_params, *args, **kwargs):
+        """
+        needed for the vec env
+        :param objective_params: this is an arg for the reset of the env! It specifies the task
+        :param args/kwargs: these are just to throw away all other reset args for the env that don't define the task
+        """
+        actions = []
+        for obs, goal in zip(observations, objective_params):
+            goal_vec = np.array(goal) - np.array(obs)
+            max_coef = max(np.max(goal_vec / self.action_space.bounds), 1)  # done to scale the action, not clip it
+            actions.append(goal_vec / max_coef)  # so that direction is maintained.
+        return actions, dict()
+
+
+def plot_policy_means(policy, env, bounds=None, report=None, num_samples=10, goals=None, idxs=np.arange(5)):
+    # use idxs=None if not vectorized, and a list if specifying what envs to plot
+    if bounds is None:
+        bounds = env.observation_space.bounds
+    x = np.linspace(bounds[0][0], bounds[1][0], num_samples)
+    y = np.linspace(bounds[0][1], bounds[1][1], num_samples)
+    states = np.stack(np.meshgrid(x, y), axis=-1).reshape(-1, 2)
+    # observations = [np.concatenate([state, [0, ] * (env.observation_space.flat_dim - len(state) - len(goal)), goal]) for
+    #                 state in states]  # in case we need to append goal for the policy
+    means = []
+    log_stds = []
+    # import pdb; pdb.set_trace()
+    for state in states:
+        actions, agent_info = policy.get_action([state] * len(goals), idx=idxs)
+        means.append(agent_info['mean'])
+        log_stds.append(agent_info['log_std'])
+    for idx in idxs:
+        vecs = np.array([mean[idx] for mean in means])
+        vars = np.array([np.exp(log_std[idx]) * 0.25 for log_std in log_stds])
+        ells = [patches.Ellipse(state, width=vars[i][0], height=vars[i][1], angle=0) for i, state in enumerate(states)]
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        for e in ells:
+            ax.add_artist(e)
+            e.set_alpha(0.2)
+        # plt.scatter(*np.array(goals)[idx], color='r', s=100)
+        plt.plot(*np.array(goals)[idx], 'r*', markersize=10)
+        plt.plot(0, 0, 'b.', markersize=5)
+        Q = plt.quiver(states[:, 0], states[:, 1], vecs[:, 0], vecs[:, 1], units='xy', angles='xy', scale_units='xy',
+                       scale=1)  # , np.linalg.norm(vars * 4)
+        qk = plt.quiverkey(Q, 0.8, 0.85, 1, r'1 Nkg', labelpos='E', coordinates='figure')
+        # cb = plt.colorbar(Q)
+        vec_img = save_image()
+        if report is not None:
+            report.add_image(vec_img, 'policy means Env_' + str(idx))
+
+
+def save_image(fig=None, fname=None):
+    if fname is None:
+        fname = tempfile.TemporaryFile()
+    if fig is not None:
+        fig.savefig(fname)
+    else:
+        plt.savefig(fname, format='png')
+    plt.close('all')
+    fname.seek(0)
+    img = scipy.misc.imread(fname)
+    fname.close()
+    return img
